41b92aec8258079f2b9668fb9a81b6cfce8fdb2e
Arthur-Sena <asassena1002@gmail.com>
2020-07-29 16:38:06 -0300

    Refeito

---
+++ OrdenarNumeros/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Nova_pasta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int []vetorNumero= new int[3];
+            int i;
+
+            for(i=0;i<3;i++) {
+                Console.Write("Digite o {0}º numero: ",i+1);
+                vetorNumero[i]=Convert.ToInt32(Console.ReadLine());
+            }
+
+            Array.Sort(vetorNumero);
+
+            System.Console.Write("A ordem é:");
+            for (i = 0; i < 3; i++) {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write($" {vetorNumero[i]}");
+                Console.ResetColor();
+            }
+        }
+    }
+}
